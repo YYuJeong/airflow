9932b8cb54d923c5cb476c5870760dcd957096bb
yujeong_yang@tmax.co.kr <yujeong_yang@tmax.co.kr>
2024-01-09 11:55:58 +0900

    bash_template dag 수정

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -19,8 +19,8 @@ with DAG(
         env ={
             'START_DATE':'{{data_interval_start | ds}}',
             'END_DATE' : '{{data_interval_end | ds}}'
-        }
-        bash_command='echo  $START_DATE && echo $END_DATE'
+        },
+        bash_command='echo $START_DATE && echo $END_DATE'
     )
     
     bash_t1 >> bash_t2
